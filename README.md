e128e4b065252abea29c363564e7d6116da0ec5f
Michael Petychakis <mpetyx@gmail.com>
2013-03-01 17:19:27 +0200

    database change

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ stamatiou
 =========
 
 the official website stamatiou.gr written in django-cms
+
+
+plugins can be found https://www.djangopackages.com/grids/g/django-cms/
--- stamatiousite/settings.py
+++ stamatiousite/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 #DATABASES = {
 #    'default': {
